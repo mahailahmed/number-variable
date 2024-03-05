56a00b820a8dbc398b0d1b18669b589b73a0d917
mahailahmed <160250995+mahailahmed@users.noreply.github.com>
2024-03-05 11:53:16 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+// Declare a number variable
+var age = 25;
+// Perform arithmetic operations
+var x = 10;
+var y = 5;
+var sum = x + y;
+var difference = x - y;
+var product = x * y;
+var quotient = x / y;
+// Print the results
+console.log(age); //output is    (25)
+console.log(sum); //output is    (15)
+console.log(difference); //output is     (5)
+console.log(product); //output is    (50)
+console.log(quotient); //output is    (2)
---
+++ main.ts
@@ -0,0 +1,17 @@
+// Declare a number variable
+let age: number = 25
+
+// Perform arithmetic operations
+let x: number = 10
+let y: number = 5
+let sum: number = x + y
+let difference: number = x - y
+let product: number = x * y
+let quotient: number = x / y
+
+// Print the results
+console.log( age)                 //output is    (25)
+console.log( sum)                 //output is    (15)
+console.log(difference)           //output is     (5)
+console.log( product)             //output is    (50)
+console.log( quotient)            //output is    (2)
